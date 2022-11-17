7ddb4a8239b2986db62b908e2f34b30aee008a51
Siarhei Kamarou <98712488+serjoeb@users.noreply.github.com>
2022-11-18 00:38:18 +0300

    Delete build directory

--- build/js/swiper.js
+++
@@ -1,1 +0,0 @@
-const swiperContainer=document.querySelector(".swiper-wrapper");swiperContainer.classList.remove("swiper-wrapper--nojs");const swiper=new Swiper(".swiper",{direction:"horizontal",pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
